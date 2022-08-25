f070b1de61a9fc3d5fd65cf53b64651c579f1cad
Thomas Roche <thomas.a.roche@gmail.com>
2022-08-25 08:23:24 +0100

    testing for contributor holding commits

--- py-documentation-generator/tests/models/test_contributor_manager.py
+++ py-documentation-generator/tests/models/test_contributor_manager.py
@@ -83,3 +83,23 @@ def test__init_contributors_excluded(faker, tmp_path_factory):
     assert len(sut) == 2
     for item in sut:
         assert isinstance(item, Contributor)
+
+
+def test__init_contributors_exists(faker, tmp_path_factory):
+    # Arrange
+    tmp_dir = tmp_path_factory.mktemp("data")
+    repo = faker.repository(dir_path=tmp_dir)
+
+    contributor = faker.contributor()
+    commit_1 = faker.commit(dir_path=tmp_dir, contributor=contributor)
+    commit_2 = faker.commit(dir_path=tmp_dir, contributor=contributor)
+
+    # Act
+    manager = ContributorManager(repo_path=repo.working_tree_dir)
+    sut = manager._get_contributor(contributor.name, contributor.email)
+
+    # Assert
+    assert sut in manager._contributors
+    assert len(sut.commits) == 2
+    # reverse order (git history)
+    assert sut.commits == [commit_2, commit_1]
